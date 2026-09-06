dfd0b002055d3d2b045f1b309e37bb94039ac828
akashsaron <56619101+akashsaron@users.noreply.github.com>
2026-09-06 17:00:34 +0530

    Delete lib directory

--- lib/Page.tsx
+++
@@ -1,2 +0,0 @@
-import { pageContent } from './page-content';
-export default function Page({name}:{name:string}){return <main dangerouslySetInnerHTML={{__html: pageContent[name] || pageContent['404']}}/>}
